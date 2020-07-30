55278e5b5e022bc740fa58abab8d18838e6a600b
Jeanette Payne <jeanetterpayne@gmail.com>
2020-07-30 00:23:58 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,50 @@
+class TicTacToe
+  
+  WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,4,8],
+    [2,4,6],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8]
+    ]
+  
+  def initialize(board = nil)
+    @board = board || Array.new(9, " ")
+  end
+  
+  def display_board
+  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+  puts "-----------"
+  puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+  puts "-----------"
+  puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+end
+
+def input_to_index(number)
+ number.to_i - 1
+end
+
+def move(index, token = "X")
+  @board[index] = token
+end
+
+def position_taken?(index)
+  if @board[index] == " " || @board[index] == "" || @board [index] == nil
+    FALSE
+  else
+    TRUE
+  end
+end
+
+def valid_move?(board, index)
+  if index.between?(0, 8) && board[index] != "X" && board[index] != "O"
+    TRUE
+  else
+    FALSE
+  end
+end
+  
+end
